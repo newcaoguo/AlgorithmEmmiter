de696edf97c032d5264aa442dc0ba7f9854af219
Newcaoguo <newcaoguo@foxmail.com>
2018-08-12 22:43:45 +0800

    Add files via upload

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+public class MaximumDepthOfBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        else {
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+    }
+    
+}
