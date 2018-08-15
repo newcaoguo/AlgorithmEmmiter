7902ec6bd662d4746cc79878ae17e3ac6c3edb71
Newcaoguo <newcaoguo@foxmail.com>
2018-08-15 22:05:40 +0800

    Add files via upload

---
+++ ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,32 @@
+/**
+ * 把一个有序的数组转换成二叉搜索树
+ *
+ * @author newcaoguo
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, left, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, right);
+        return root;
+    }
+
+}
