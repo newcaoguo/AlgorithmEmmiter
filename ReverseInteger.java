7c44ac7df18d1666c805aaefed42fb347b6c8f00
Newcaoguo <newcaoguo@foxmail.com>
2018-08-23 19:15:27 +0800

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,23 @@
+public class ReverseInteger {
+
+    // 123  ->  321
+    // -123  -> -321
+    // 120  ->  21
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (result > (Integer.MAX_VALUE / 10)
+                    || ((result == Integer.MAX_VALUE / 10) && pop > 7)) return 0;
+            if (result < (Integer.MIN_VALUE / 10)
+                    || ((result == Integer.MIN_VALUE / 10) && pop < -8)) return 0;
+            result = result * 10 + pop;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
