6e54dea9ca4a728893d463893ec3731c854943a6
Newcaoguo <newcaoguo@foxmail.com>
2018-08-26 15:37:35 +0800

    Add files via upload

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"a","b","d"}));
+    }
+}
