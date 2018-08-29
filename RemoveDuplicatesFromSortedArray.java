69592219012b6c08b903982df89372989474d569
Newcaoguo <newcaoguo@foxmail.com>
2018-08-29 21:34:53 +0800

    删除有序数组中的重复数据

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+    }
+}
