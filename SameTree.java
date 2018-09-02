5f3f47f23e51293395c2ba3f970648fc66a311db
Newcaoguo <newcaoguo@foxmail.com>
2018-09-02 23:17:13 +0800

    Add files via upload

---
+++ SameTree.java
@@ -0,0 +1,20 @@
+public class SameTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        else
+            return false;
+    }
+}
