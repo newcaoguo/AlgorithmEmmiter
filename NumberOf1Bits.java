3eb82c60f30ec35677e2035944f8e12e4d515efc
Newcaoguo <newcaoguo@foxmail.com>
2018-09-24 08:30:54 +0800

    Add files via upload

---
+++ NumberOf1Bits.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class NumberOf1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int bits = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask <<= 1;
+        }
+        return bits;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(hammingWeight(11));
+    }
+}
