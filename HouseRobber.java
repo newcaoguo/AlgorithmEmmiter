676292ac8d5e9d108dbcf6f6c660efe6621ad04a
Newcaoguo <newcaoguo@foxmail.com>
2018-10-08 13:20:40 +0800

    Update HouseRobber.java

--- HouseRobber.java
+++ HouseRobber.java
@@ -24,5 +24,19 @@ public class HouseRobber {
         if (nums.length == 0) return 0;
         return robot(0, nums);
     }
+    
+    public int rob(int[] nums){
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        result[1] = Math.max(nums[0], nums[1]);
+        for(int index = 2; index < nums.length; index++){
+            result[index] = Math
+                .max(nums[index] + result[index - 2], result[index - 1]);
+        }
+        return result[nums.length - 1];
+    }
 
 }
