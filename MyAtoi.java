60eca34b38b63eb2580a7730e276243c55ed38b5
Newcaoguo <newcaoguo@foxmail.com>
2018-11-30 15:40:30 +0800

    Add files via upload

---
+++ MyAtoi.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class MyAtoi {
+
+    public int myAtoi(String str) {
+        if (str == null) return 0;
+        str = str.trim();
+        if (str.length() == 0) return 0;
+
+        int sign = 1;
+        int index = 0;
+
+        if (str.charAt(index) == '+') {
+            index++;
+        } else if (str.charAt(index) == '-') {
+            sign = -1;
+            index++;
+        }
+        long num = 0;
+        for (; index < str.length(); index++) {
+            if (str.charAt(index) < '0' || str.charAt(index) > '9')
+                break;
+            num = num * 10 + (str.charAt(index) - '0');
+            if (num > Integer.MAX_VALUE) {
+                break;
+            }
+        }
+        if (num * sign >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (num * sign <= Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int) num * sign;
+    }
+}
