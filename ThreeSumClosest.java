a4edb5346464610f7cf1fa399ab3b885f836668b
Newcaoguo <newcaoguo@foxmail.com>
2018-12-05 18:29:24 +0800

    Add files via upload

---
+++ ThreeSumClosest.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closest = target - nums[0] - nums[1] - nums[2];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            int newTarget = target - nums[i];
+
+            while (l < r) {
+                if (nums[l] + nums[r] == newTarget) return target;
+
+                int diff = newTarget - nums[l] - nums[r];
+                if (Math.abs(diff) < Math.abs(closest)) closest = diff;
+
+                if (nums[l] + nums[r] < newTarget) l++;
+                else r--;
+            }
+        }
+
+        return target - closest;
+    }
+}
