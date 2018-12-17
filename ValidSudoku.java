87cb2c01ff1e5e011e4514bab8c7218256140998
Newcaoguo <newcaoguo@gmail.com>
2018-12-17 14:56:17 +0800

    Update ValidSudoku.java

--- ValidSudoku.java
+++ ValidSudoku.java
@@ -5,26 +5,19 @@ import org.junit.Test;
 public class ValidSudoku {
 
     public boolean isValidSudoku(char[][] board) {
-        if (board.length == 0 || board[0].length == 0) return false;
-        int m = board.length;
-        int n = board[0].length;
-        boolean[][] rowFlag = new boolean[m][n];
-        boolean[][] colFlag = new boolean[m][n];
-        boolean[][] cellFlag = new boolean[m][n];
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] >= '1' && board[i][j] <= '9') {
-                    int c = board[i][j] - '1'; // 4
-                    if (rowFlag[i][c] || // row[0][4]
-                            colFlag[c][j] || // col[4][0]
-                            cellFlag[3 * (i / 3) + j / 3][c]) // cell[0][4]
+        int[][] rows = new int[9][9];
+        int[][] cols = new int[9][9];
+        int[][] boxs = new int[9][9];
+
+        for(int i = 0; i < board.length; ++ i)
+            for(int j = 0; j < board[i].length; ++ j)
+                if(board[i][j] != '.') {
+                    int num = board[i][j] - '0' - 1;
+                    int k = i / 3 * 3 + j / 3;
+                    if(rows[i][num]==1 || cols[j][num]==1 || boxs[k][num]==1)
                         return false;
-                    rowFlag[i][c] = true;
-                    colFlag[c][j] = true;
-                    cellFlag[3 * (i / 3) + j / 3][c] = true;
+                    rows[i][num] = cols[j][num] = boxs[k][num] = 1;
                 }
-            }
-        }
         return true;
     }
 
