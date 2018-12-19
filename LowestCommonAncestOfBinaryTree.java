51990c2a6e1da9a2742622bed1c837a471d738ef
Newcaoguo <newcaoguo@gmail.com>
2018-12-19 22:57:27 +0800

    Add files via upload

---
+++ LowestCommonAncestOfBinaryTree.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class LowestCommonAncestOfBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        if (right != null) return right;
+        return null;
+    }
+}
