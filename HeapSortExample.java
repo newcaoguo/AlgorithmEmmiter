7a521258ebe480ec63303020876c3734fb584639
Newcaoguo <newcaoguo@gmail.com>
2018-12-21 12:39:14 +0800

    更加清晰的一种实现
    更加清晰的一种实现

--- HeapSortExample.java
+++ HeapSortExample.java
@@ -8,6 +8,49 @@ public class HeapSortExample {
         System.out.println(Arrays.toString(arr));
     }
 
+    public void swap(int[] array, int i, int j) {
+        array[i] ^= array[j];
+        array[j] ^= array[i];
+        array[i] ^= array[j];
+    }
+
+    public void heapify(int[] array, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+            largest = left;
+        if (right < n && array[right] > array[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(array, i, largest);
+            heapify(array, n, largest);
+        }
+    }
+
+
+    public void heapSort(int[] array, int n) {
+        //1.构建大顶堆
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+        //2.调整堆结构+交换堆顶元素与末尾元素
+        for (int i = n - 1; i > 0; i--) {
+            swap(array, 0, i);
+            heapify(array, i, 0);
+        }
+    }
+
+
+    @Test
+    public void test() {
+        int[] array = new int[]{5, 3, 8, 9};
+        heapSort(array, array.length);
+        System.out.println(Arrays.toString(array));
+    }
+    /*
     public static void sort(int[] arr) {
         //1.构建大顶堆
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
@@ -58,5 +101,5 @@ public class HeapSortExample {
         arr[a] = arr[b];
         arr[b] = temp;
     }
-
+    */
 }
