727c60d9874205144b1560aaa090ee4d9c891bb6
Newcaoguo <newcaoguo@gmail.com>
2018-12-29 11:39:57 +0800

    Add files via upload

---
+++ JumpGame.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class JumpGame {
+
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) return false;
+            max = Math.max(nums[i] + i, max);
+        }
+        return true;
+    }
+}
