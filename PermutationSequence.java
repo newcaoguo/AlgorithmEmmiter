2b815ba39d641c59f7bb9a0036978a897477e825
Newcaoguo <newcaoguo@gmail.com>
2019-01-04 09:31:21 +0800

    Update PermutationSequence.java

--- PermutationSequence.java
+++ PermutationSequence.java
@@ -7,41 +7,25 @@ import java.util.List;
 
 public class PermutationSequence {
 
-    StringBuilder res = new StringBuilder();
-    int count = 0;
-
     public String getPermutation(int n, int k) {
-        List<List<Integer>> mList = new ArrayList<>();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) nums[i] = i + 1;
-        backtrack(mList, new ArrayList<>(), nums, new boolean[nums.length], k);
-        return res.toString();
-    }
-
-    private void backtrack(List<List<Integer>> list,
-                           List<Integer> tempList,
-                           int[] nums,
-                           boolean[] used, int k) {
-        if (tempList.size() == nums.length) {
-            if (k == ++count) {
-                for (Integer a : tempList) res.append(a);
-//                System.out.println(res);
-                return;
-            }
-            list.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (used[i] || (i > 0
-                        && nums[i] == nums[i - 1]
-                        && !used[i - 1]))
-                    continue;
-                used[i] = true;
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums, used, k);
-                used[i] = false;
-                tempList.remove(tempList.size() - 1);
-            }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            res.add(i);
         }
+        int[] fact = new int[n];
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) {
+            fact[i] = fact[i - 1] * i;
+        }
+        k = k - 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i > 0; i--) {
+            int index = k / fact[i - 1];
+            k = k % fact[i - 1];
+            sb.append(res.get(index));
+            res.remove(index);
+        }
+        return sb.toString();
     }
 
     @Test
