56e8f0e7b0e3ff3b7c50f709ef8b90b76dc1f8fd
Newcaoguo <newcaoguo@gmail.com>
2019-05-16 12:14:15 +0800

    Update HeapSortExample.java

--- HeapSortExample.java
+++ HeapSortExample.java
@@ -1,47 +1,70 @@
-import java.util.Arrays;
-
-public class HeapSortExample {
-
-    public void swap(int[] array, int i, int j) {
-        array[i] ^= array[j];
-        array[j] ^= array[i];
-        array[i] ^= array[j];
-    }
-
-    public void heapify(int[] array, int n, int i) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-
-        if (left < n && array[left] > array[largest])
-            largest = left;
-        if (right < n && array[right] > array[largest])
-            largest = right;
-
-        if (largest != i) {
-            swap(array, i, largest);
-            heapify(array, n, largest);
+public int[] topK(int[] mData, int k) {
+        if (k > mData.length)
+            throw new IllegalArgumentException("");
+        int[] kData = new int[k];
+        //1.初始化数据
+        for (int i = 0; i < kData.length; i++) {
+            kData[i] = Integer.MAX_VALUE;
         }
+        //2.
+        for (int i = 0; i < mData.length; i++) {
+            if (mData[i] < kData[0]) {
+                kData[0] = mData[i];
+                adjustHeap(kData, 0, kData.length);
+            }
+        }
+        return kData;
     }
 
-
-    public void heapSort(int[] array, int n) {
+    public static void heapSort(int[] arr) {
         //1.构建大顶堆
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(array, n, i);
+        for (int i = (arr.length - 1) / 2; i >= 0; i--) {
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
         }
-        //2.调整堆结构+交换堆顶元素与末尾元素
-        for (int i = n - 1; i > 0; i--) {
-            swap(array, 0, i);
-            heapify(array, i, 0);
+        //2.交换堆顶元素与末尾元素,并且重新调整堆结构,
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i);//将堆顶元素与末尾元素进行交换
+            adjustHeap(arr, 0, i);//重新对堆进行调整
         }
+    }
+
+    /**
+     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     */
+    public static void adjustHeap(int[] arr, int i, int length) {
+        int temp = arr[i];//先取出当前元素i
+        //从i结点的左子结点开始，也就是2i+1处开始
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
+            //如果左子结点小于右子结点，k指向右子结点
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
+                k++;
+            }
+            if (arr[k] > temp) {
+                //如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;//将 temp 值放到最终的位置
+    }
+
+    /**
+     * 交换元素
+     */
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
     @Test
     public void test() {
-        int[] array = new int[]{5, 3, 8, 9};
-        heapSort(array, array.length);
-        System.out.println(Arrays.toString(array));
+        int[] data = new int[]{3, 5, 2, 1, 9};
+        heapSort(data);
+        System.out.println(Arrays.toString(data));
+        int[] topK = topK(data, 5);
+        System.out.println(Arrays.toString(topK));
     }
-  
-}
